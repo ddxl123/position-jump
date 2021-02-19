e71bf27c5b861deaeb1d590e8545f42a4382e85a
林珑 <5598856+lin_long@user.noreply.gitee.com>
2021-02-19 15:58:44 +0800

    修改了路径方式

--- out/extension.js
+++ out/extension.js
@@ -4,7 +4,6 @@ exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-const path = require("path");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
@@ -13,18 +12,22 @@ function activate(context) {
 }
 exports.activate = activate;
 function provideDefinition(document, position, token) {
-    const fileName = document.fileName;
-    const workDir = path.dirname(fileName);
-    const word = document.getText(document.getWordRangeAtPosition(position));
-    const line = document.lineAt(position);
-    //   console.log("fileName: " + fileName); // 当前文件完整路径
-    console.log("workDir: " + workDir); // 当前文件所在目录
-    //   console.log("word: " + word); // 当前光标所在单词
-    //   console.log("line: " + line.text); // 当前光标所在行
-    // 获取 @see 之后的路径
+    var _a;
+    const fileName = document.fileName; // 当前文件完整路径
+    // const workDir = path.dirname(fileName);// 当前文件所在目录
+    // const word = document.getText(document.getWordRangeAtPosition(position));// 当前光标所在单词
+    const line = document.lineAt(position); // 当前光标所在行
+    // console.log("fileName: " + fileName);
+    // console.log("workDir: " + workDir);
+    // console.log("word: " + word);
+    // console.log("line: " + line.text);
+    // 获取项目根目录
+    const rootPath = (_a = vscode.workspace.getWorkspaceFolder(vscode.Uri.file(fileName))) === null || _a === void 0 ? void 0 : _a.uri.path;
+    // 获取 @see 之后的文本
     var index = line.text.search("@see");
     if (index != -1) {
-        var targetPath = workDir + "/" + line.text.substring(index + 5, line.text.length);
+        // 补全路径
+        var targetPath = rootPath + "/" + line.text.substring(index + 5, line.text.length);
         // console.log(targetPath);
         return new vscode.Location(vscode.Uri.file(targetPath), new vscode.Position(0, 0));
     }
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import path = require("path");
+import { env } from "process";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -18,21 +19,27 @@ function provideDefinition(
   position: vscode.Position,
   token: vscode.CancellationToken
 ) {
-  const fileName = document.fileName;
-  const workDir = path.dirname(fileName);
-  const word = document.getText(document.getWordRangeAtPosition(position));
-  const line = document.lineAt(position);
+  const fileName = document.fileName; // 当前文件完整路径
+  // const workDir = path.dirname(fileName);// 当前文件所在目录
+  // const word = document.getText(document.getWordRangeAtPosition(position));// 当前光标所在单词
+  const line = document.lineAt(position); // 当前光标所在行
 
-  //   console.log("fileName: " + fileName); // 当前文件完整路径
-  console.log("workDir: " + workDir); // 当前文件所在目录
-  //   console.log("word: " + word); // 当前光标所在单词
-  //   console.log("line: " + line.text); // 当前光标所在行
+  // console.log("fileName: " + fileName);
+  // console.log("workDir: " + workDir);
+  // console.log("word: " + word);
+  // console.log("line: " + line.text);
 
-  // 获取 @see 之后的路径
+  // 获取项目根目录
+  const rootPath = vscode.workspace.getWorkspaceFolder(
+    vscode.Uri.file(fileName)
+  )?.uri.path;
+
+  // 获取 @see 之后的文本
   var index = line.text.search("@see");
   if (index != -1) {
+    // 补全路径
     var targetPath =
-      workDir + "/" + line.text.substring(index + 5, line.text.length);
+      rootPath + "/" + line.text.substring(index + 5, line.text.length);
     // console.log(targetPath);
     return new vscode.Location(
       vscode.Uri.file(targetPath),
